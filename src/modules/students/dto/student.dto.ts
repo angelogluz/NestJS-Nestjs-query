af23aeb6840a84cf2b771880ea03103ce735c69d
Angelo Gonçalves da Luz <angelogl@gmail.com>
2021-10-11 18:13:10 -0300

    :recycle: Relate student with discipline

--- src/modules/students/dto/student.dto.ts
+++ src/modules/students/dto/student.dto.ts
@@ -1,11 +1,16 @@
-import { FilterableField } from '@nestjs-query/query-graphql';
-import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  FilterableField,
+  FilterableOffsetConnection,
+} from '@nestjs-query/query-graphql';
+import { ObjectType } from '@nestjs/graphql';
+import { BaseDTO } from 'src/modules/bases/dto/base.dto';
+import { DisciplineDTO } from 'src/modules/disciplines/dto/discipline.dto';
 
 @ObjectType('Student')
-export class StudentDTO {
-  @Field()
-  id: string;
-
+@FilterableOffsetConnection('disciplines', () => DisciplineDTO, {
+  nullable: true,
+})
+export class StudentDTO extends BaseDTO {
   @FilterableField()
   name: string;
 
