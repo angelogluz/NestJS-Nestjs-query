6a533d2a52e39dbb23840a010d9264a2a5b315ae
Angelo Gonçalves da Luz <angelogl@gmail.com>
2021-10-11 18:11:32 -0300

    :art: Remove unnecessary decorators

--- src/modules/students/dto/create-student.input.ts
+++ src/modules/students/dto/create-student.input.ts
@@ -1,10 +1,8 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateStudentInput {
-  @Field()
   name: string;
 
-  @Field()
   key: string;
 }
--- src/modules/students/entities/student.entity.ts
+++ src/modules/students/entities/student.entity.ts
@@ -1,13 +1,17 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Discipline } from 'src/modules/disciplines/entities/discipline.entity';
+import { Column, Entity, ManyToMany } from 'typeorm';
+import { BaseEntity } from './../../bases/entities/base.entity';
 
 @Entity()
-export class Student {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
+export class Student extends BaseEntity {
   @Column()
   name: string;
 
   @Column()
   key: string;
+
+  @ManyToMany(() => Discipline, (disciplines) => disciplines.students, {
+    nullable: true,
+  })
+  disciplines: Discipline[];
 }
